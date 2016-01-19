0b042fe1314f5b0456d4fc2cff6820956b3f2527
BurizaDo <mengyi.xu@imike.com>
2016-01-19 19:27:22 +0800

    update msg, move bi downstairs

--- src/main/java/WeixinMsgController.java
+++ src/main/java/WeixinMsgController.java
@@ -113,13 +113,11 @@ public class WeixinMsgController extends MsgControllerAdapter {
 	 */
 	protected void processInFollowEvent(InFollowEvent inFollowEvent)
 	{
-		//BI统计
-//		BIFollowEventApi.sendFollowEvent(inFollowEvent);
 		//处理
 		if (InFollowEvent.EVENT_INFOLLOW_SUBSCRIBE.equals(inFollowEvent.getEvent()))
 		{
 			logger.debug("关注：" + inFollowEvent.getFromUserName());
-			String text = "感谢关注眯客~\r\n注册眯客,全天特价,住酒店30元起,下载app评价还有返现哦~<a href=\"http://www.imike.com\">现在验证手机,即可获酒店大红包.</a>";
+			String text = "感谢关注眯客~\r\n\r\n注册眯客,全天特价,住酒店30元起,下载app评价还有返现哦~\r\n\r\n<a href=\"http://www.imike.com\">现在验证手机,即可获酒店大红包。</a>";
 			renderOutTextMsg(text);
 		}
 		// 如果为取消关注事件，将无法接收到传回的信息
@@ -127,6 +125,8 @@ public class WeixinMsgController extends MsgControllerAdapter {
 		{
 			logger.debug("取消关注：" + inFollowEvent.getFromUserName());
 		}
+		//BI统计
+		BIFollowEventApi.sendFollowEvent(inFollowEvent);
 	}
 
 	@Override
